25a0f907974d37258e6e62282d41566165253535
busesorin94 <sorin-marian.buse@my.fmi.unibuc.ro>
2016-01-16 02:15:57 +0200

    Made database

--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def index(request):
-    return HttpResponse("Rango says hey there world!")
+class IndexView(TemplateView):
+    template_name="index.html"
